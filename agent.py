a9a9dcd43ca1c5dc3d0a4a8fcdeb54610db3fadb
Pierre Ramez <pierre.ramez10@gmail.com>
2026-01-15 17:30:04 +0200

    made some changes to the variable __name__ inside agent.py to be able to pass the CI tests

--- agent.py
+++ agent.py
@@ -94,73 +94,73 @@ def analyze_with_gemini(description):
     
 
 # Main Program
+if __name__== "__main__":
+  seen_cves = set()
+  approved_cves=dict()
+  dashboard_data=dict()
+  NVD_API = os.environ.get('NVD_API_KEY')
+  while True:
+    end = datetime.datetime.now()
+    start = end - datetime.timedelta(days=1)
+    r = searchCVE(pubStartDate=start, pubEndDate=end, key=NVD_API)
+    print(f"Fetched {len(r)} records.")
 
-seen_cves = set()
-approved_cves=dict()
-dashboard_data=dict()
-NVD_API = os.environ.get('NVD_API_KEY')
-while True:
-  end = datetime.datetime.now()
-  start = end - datetime.timedelta(days=1)
-  r = searchCVE(pubStartDate=start, pubEndDate=end, key=NVD_API)
-  print(f"Fetched {len(r)} records.")
+    for cve in r[:5]: # Print first 5 to check
+        print(f"{cve.id}: {cve.descriptions[0].value}")
+    for cve in r:
+      if cve.id not in seen_cves and cve.vulnStatus != "Rejected": #check if this is a new cve and wasn't rejected as a vulnerability
+        seen_cves.add(cve.id) #to avoid reprocessing the same cve
 
-  for cve in r[:5]: # Print first 5 to check
-      print(f"{cve.id}: {cve.descriptions[0].value}")
-  for cve in r:
-    if cve.id not in seen_cves and cve.vulnStatus != "Rejected": #check if this is a new cve and wasn't rejected as a vulnerability
-      seen_cves.add(cve.id) #to avoid reprocessing the same cve
-
-      try:
-        description = cve.descriptions[0].value #to get the description
-        if is_potential_ot(description):  # check if the CVE can be OT related
-            print(f"New potential threat is detected: {cve.id}")
-            print(f"sending {cve.id} description to LLM for analysis...")
-            response = analyze_with_gemini(description)
-            time.sleep(5) # making sure to wait 20 seconds between each check to prevent hitting the API limit
-            
-            if response['ot_related'] == True: # check if the response is OT related to keep/ignore it
-              print(f"""
-              ###################################
-              APPROVED: {cve.id} is an OT threat!
-              ###################################
-              """)
-
-              try:
-                cvss = cve.metrics.cvssMetricV31[0].cvssData.baseScore #new CVEs might not have a severity score. Implemented a safety check
-                severity = cve.metrics.cvssMetricV31[0].cvssData.baseSeverity
-              except:
-                cvss = 'N/A' # if no severity score. set the severity value to 'N/A'
-                severity = 'N/A'
-              #initialized this to save every new approved CVE
-              approved_cves[cve.id] = {'cvss':cvss,
-                                       'severity':severity,
-                                       'description':f"{description}",
-                                       'ai_insight':f'{response['reason']}'}
-              new_entry = {'cve_id':f'{cve.id}'}|approved_cves[cve.id] #concatenating the dict to output a separate new output to pass to the dashboard
+        try:
+          description = cve.descriptions[0].value #to get the description
+          if is_potential_ot(description):  # check if the CVE can be OT related
+              print(f"New potential threat is detected: {cve.id}")
+              print(f"sending {cve.id} description to LLM for analysis...")
+              response = analyze_with_gemini(description)
+              time.sleep(5) # making sure to wait 20 seconds between each check to prevent hitting the API limit
               
-              # output the OT threat found to a JSON file
-              try:
-                with open('output_sample.json',mode='r') as f: #used mode=a to append to existing file and not overwrite existing data
-                  data = json.load(f)
-                  
-                  # checking and correcting the data type
-                  if isinstance(data,dict):
-                    data = [data]
-                  elif not isinstance(data,list):
-                    data = []
+              if response['ot_related'] == True: # check if the response is OT related to keep/ignore it
+                print(f"""
+                ###################################
+                APPROVED: {cve.id} is an OT threat!
+                ###################################
+                """)
 
-              except:
-                data = []
-              
-              data.append(new_entry)
+                try:
+                  cvss = cve.metrics.cvssMetricV31[0].cvssData.baseScore #new CVEs might not have a severity score. Implemented a safety check
+                  severity = cve.metrics.cvssMetricV31[0].cvssData.baseSeverity
+                except:
+                  cvss = 'N/A' # if no severity score. set the severity value to 'N/A'
+                  severity = 'N/A'
+                #initialized this to save every new approved CVE
+                approved_cves[cve.id] = {'cvss':cvss,
+                                        'severity':severity,
+                                        'description':f"{description}",
+                                        'ai_insight':f'{response['reason']}'}
+                new_entry = {'cve_id':f'{cve.id}'}|approved_cves[cve.id] #concatenating the dict to output a separate new output to pass to the dashboard
+                
+                # output the OT threat found to a JSON file
+                try:
+                  with open('output_sample.json',mode='r') as f: #used mode=a to append to existing file and not overwrite existing data
+                    data = json.load(f)
+                    
+                    # checking and correcting the data type
+                    if isinstance(data,dict):
+                      data = [data]
+                    elif not isinstance(data,list):
+                      data = []
 
-              if data:
-                with open('output_sample.json',mode='w') as f:
-                  json.dump(data,f,indent=4)
-        else:
+                except:
+                  data = []
+                
+                data.append(new_entry)
+
+                if data:
+                  with open('output_sample.json',mode='w') as f:
+                    json.dump(data,f,indent=4)
+          else:
+            continue
+
+        except:
           continue
-
-      except:
-        continue
-  time.sleep(600)
+    time.sleep(600)
